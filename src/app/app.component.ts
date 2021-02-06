04fda5e1fac68c6481634c9c04a915a5b2fbc29d
Oleksii Baklanov <oleksii_baklanov@epam.com>
2021-02-07 01:22:45 +0200

    [OB] removed tests, added clock, added note form and json-data presentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'My project';
+export class AppComponent implements OnInit, OnDestroy {
+  time: Date;
+  notes: any;
+  timer!: NodeJS.Timeout;
+
+  constructor() {
+    this.time = new Date();
+    this.notes = [
+      {
+        'author': 'Name1',
+        'message': 'message1'
+      },
+      {
+        'author': 'Name2',
+        'message': 'message2'
+      },
+      {
+        'author': 'Name3',
+        'message': 'message3'
+      }
+    ];
+  }
+
+  ngOnInit() {
+    this.timer = setInterval(() => {
+      this.time = new Date();
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timer);
+  }
 }
