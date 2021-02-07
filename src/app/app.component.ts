a72239c93909ca1c7453adccf5518a6b912f978b
Oleksii Baklanov <oleksii_baklanov@epam.com>
2021-02-07 18:45:35 +0200

    [OB] added some bootstrap styles, added form for adding new notes, added availability to fill author and title of note by clicking on existing note

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,25 @@ export class AppComponent implements OnInit, OnDestroy {
   time: Date;
   notes: any;
   timer!: NodeJS.Timeout;
+  noteAuthor: string = '';
+  noteTitle: string = '';
 
   constructor() {
     this.time = new Date();
     this.notes = [
       {
         'author': 'Name1',
+        'title': 'Title1',
         'message': 'message1'
       },
       {
         'author': 'Name2',
+        'title': 'Title2',
         'message': 'message2'
       },
       {
         'author': 'Name3',
+        'title': 'Title3',
         'message': 'message3'
       }
     ];
@@ -36,5 +41,19 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     clearInterval(this.timer);
+  }
+
+  showNote($event: MouseEvent, note: any) {
+    this.noteAuthor = note.author;
+    this.noteTitle = note.title;
+    note.highlight = !note.highlight;
+  }
+
+  clearNoteAuthor($event: KeyboardEvent) {
+    this.noteAuthor = '';
+  }
+
+  clearNoteTitle($event: KeyboardEvent) {
+    this.noteTitle = '';
   }
 }
